ec4d150bc560eb6eed8bdbff9de844eda660a16e
nookh <130391869+nookh@users.noreply.github.com>
2024-07-13 08:35:11 +0500

    Update Footer.jsx

--- src/components/Header/Footer.jsx
+++ src/components/Header/Footer.jsx
@@ -1,27 +1,12 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import BottomNavigation from '@mui/material/BottomNavigation';
-import BottomNavigationAction from '@mui/material/BottomNavigationAction';
-import RestoreIcon from '@mui/icons-material/Restore';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import LocationOnIcon from '@mui/icons-material/LocationOn';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-export default function SimpleBottomNavigation() {
-  const [value, setValue] = React.useState(0);
+const Tab = createBottomTabNavigator();
 
+function MyTabs() {
   return (
-    <Box sx={{ width: 500 }}>
-      <BottomNavigation
-        showLabels
-        value={value}
-        onChange={(event, newValue) => {
-          setValue(newValue);
-        }}
-      >
-        <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
-        <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
-        <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
-      </BottomNavigation>
-    </Box>
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Settings" component={SettingsScreen} />
+    </Tab.Navigator>
   );
 }

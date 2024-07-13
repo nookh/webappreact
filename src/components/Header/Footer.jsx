028ec65d8471669772573eeb123bbb93426c5571
nookh <130391869+nookh@users.noreply.github.com>
2024-07-13 08:33:24 +0500

    Update Footer.jsx

--- src/components/Header/Footer.jsx
+++ src/components/Header/Footer.jsx
@@ -1,18 +1,27 @@
-import React from 'react';
-import Button from "../Button/Button";
-import {useTelegram} from "../../hooks/useTelegram";
-import './Header.css';
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import RestoreIcon from '@mui/icons-material/Restore';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
 
-const Header = () => {
-    const {user, onClose} = useTelegram();
-    console.log(user);
-    return (
-        <div className={'header'}>
-            <span className={'username'}>
-              {user?.first_name}
-            </span>
-        </div>
-    );
-};
+export default function SimpleBottomNavigation() {
+  const [value, setValue] = React.useState(0);
 
-export default Header;
+  return (
+    <Box sx={{ width: 500 }}>
+      <BottomNavigation
+        showLabels
+        value={value}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+      >
+        <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
+        <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
+        <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
+      </BottomNavigation>
+    </Box>
+  );
+}
